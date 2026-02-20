70b65b8b2c4b13b7610f9a58104f5026e811bf26
Em-driod <sheenleen2@gmail.com>
2026-02-20 16:43:48 +0100

    sss

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { FoodItem, ProteinOption } from '../types';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://atmosfoodin.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
